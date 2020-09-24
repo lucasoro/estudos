9e25327675476d025336b6fce8d71b044977c3e4
oro <lucasoro97@gmail.com>
2020-09-23 22:53:37 -0300

    Em desenvolvimento - Lista duplamente encadeada

---
+++ classes/Imperativa/listaDupEncadeada/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definição da estrutura de nó
+struct node {
+	struct node* prev;
+	struct node* next;
+	int val;
+};
+
+// Definições utilizadas para simplicidade no código
+typedef struct node no;
+typedef struct node* ponteiro_no;
+
+// Método para inserção de nós na lista
+void insere_no(ponteiro_no ponteiro) {
+
+}
+
+// Método para a impressão da lista
+void imprime_lista(ponteiro_no ponteiro) {
+
+}
+
+// Método main - Principal
+int main() {
+	return 0;
+}
